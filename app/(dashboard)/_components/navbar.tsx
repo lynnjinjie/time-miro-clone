aaaa877f79e92f67efe160ea60c726714c3bc840
lynnjiang <lynnjinjie@gmail.com>
2024-04-06 23:44:19 +0800

    feat: init and add clerk and convex

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { UserButton } from '@clerk/nextjs'
+
+export function Navbar() {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg:flex lg:flex-1 bg-orange-500">search</div>
+      <UserButton />
+    </div>
+  )
+}
